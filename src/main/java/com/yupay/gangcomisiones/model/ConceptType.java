d6c49240ba5e2dca44ba74ed5a44565b9347a48d
dvidal <dvidal@infoyupay.com>
2025-09-14 00:21:06 -0500

    feat: add `validateCommissionValue` method to `ConceptType`
    Implemented `validateCommissionValue` for `FIXED` and `RATE` enums to ensure commission values meet defined criteria. Updated JavaDocs to clarify usage and constraints for commission validation.

--- src/main/java/com/yupay/gangcomisiones/model/ConceptType.java
+++ src/main/java/com/yupay/gangcomisiones/model/ConceptType.java
@@ -41,6 +41,11 @@ public enum ConceptType {
         public BigDecimal computeCommission(@NotNull BigDecimal amount, @NotNull BigDecimal value) {
             return value;
         }
+
+        @Override
+        public boolean validateCommissionValue(@NotNull BigDecimal value) {
+            return value.compareTo(BigDecimal.ZERO) > 0 && value.compareTo(new BigDecimal("100")) < 0;
+        }
     },
     /**
      * Represents a percentage-based commission type,
@@ -50,6 +55,11 @@ public enum ConceptType {
         @Override
         public BigDecimal computeCommission(@NotNull BigDecimal amount, @NotNull BigDecimal value) {
             return amount.multiply(value).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        @Override
+        public boolean validateCommissionValue(@NotNull BigDecimal value) {
+            return value.compareTo(BigDecimal.ZERO) > 0 && value.compareTo(BigDecimal.ONE) < 0;
         }
     };
 
@@ -68,18 +78,44 @@ public enum ConceptType {
      *                 <li>In percentage-based commissions, this value is used as the base for the calculation.</li>
      *                 <li>In fixed commissions, this value is not used directly in the computation.</li>
      *               </ol>
-     * @param value The rate or fixed value of the commission. Must not be null.
-     *              <br/>Examples:
-     *              <ul>
-     *                <li>In fixed commissions, this represents the predefined fixed amount to return.</li>
-     *                <li>In percentage-based commissions, this represents the rate at which the amount is calculated (e.g., 0.05 for 5%).</li>
-     *              </ul>
+     * @param value  The rate or fixed value of the commission. Must not be null.
+     *               <br/>Examples:
+     *               <ul>
+     *                 <li>In fixed commissions, this represents the predefined fixed amount to return.</li>
+     *                 <li>In percentage-based commissions, this represents the rate at which the amount is calculated (e.g., 0.05 for 5%).</li>
+     *               </ul>
      * @return The computed commission as a {@link BigDecimal}, according to the commission type.
-     *         <br/>
-     *         <ul>
-     *           <li>For a fixed commission type, the value is returned as-is.</li>
-     *           <li>For a rate commission type, the calculated percentage of the amount is returned.</li>
-     *         </ul>
+     * <br/>
+     * <ul>
+     *   <li>For a fixed commission type, the value is returned as-is.</li>
+     *   <li>For a rate commission type, the calculated percentage of the amount is returned.</li>
+     * </ul>
      */
-    public abstract BigDecimal computeCommission(BigDecimal amount, BigDecimal value);
+    public abstract BigDecimal computeCommission(@NotNull BigDecimal amount, @NotNull BigDecimal value);
+
+    /**
+     * Validates the given commission value based on specific criteria.
+     * This method is designed to ensure that the value is suitable for use
+     * as a commission, depending on the commission type that implements it.
+     * <br/>
+     * <br/>
+     * <ul>
+     *   <li>For fixed commissions, the value must be within a specific range (e.g., greater than 0 and less than 100).</li>
+     *   <li>For rate commissions, the value must represent a valid percentage (e.g., greater than 0 and less than 1).</li>
+     * </ul>
+     *
+     * @param value The commission value to validate. Must not be null.
+     *              <br/>
+     *              Examples:
+     *              <ul>
+     *                <li>For a fixed commission type, this represents the fixed amount (e.g., 50.00).</li>
+     *                <li>For a rate commission type, this represents the percentage as a decimal (e.g., 0.05 for 5%).</li>
+     *              </ul>
+     * @return {@code true} if the provided commission value is valid according to the defined criteria; {@code false} otherwise.
+     * @implNote The fixed commission limit of 100 soles is meant to be a conservativ safeguard in Per&uacute;, which is the country
+     * where the application is primarily used. This limit ensures that the commission values are reasonable and do not exceed
+     * the typical range of commissions in the Peruvian market, plus a reasonable margin. Also, the rate commission limit
+     * of 1 is meant to prevent a 100% or superior commission rate, which is unrealistic in the scope of this app.
+     */
+    public abstract boolean validateCommissionValue(@NotNull BigDecimal value);
 }
