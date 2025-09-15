63c372a25d3c7422c0f0d49b417c5b977ace2b18
dvidal <dvidal@infoyupay.com>
2025-09-15 10:43:01 -0500

    docs: add package-level documentation for `transaction.reversal.request` use case
    Added JavaDoc and license header to describe the purpose and components of the `transaction.reversal.request` package, detailing its role in managing transaction reversal requests.

---
+++ src/main/java/com/yupay/gangcomisiones/usecase/transaction/reversal/request/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * gang-comisiones
+ * COPYLEFT 2025
+ * Ingenieria Informatica Yupay SACS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ *  with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Use case for requesting a reversal of a transaction. Contains controller, supplier and view contracts.
+ *
+ * @author InfoYupay SACS
+ * @version 1.0
+ */
+package com.yupay.gangcomisiones.usecase.transaction.reversal.request;
