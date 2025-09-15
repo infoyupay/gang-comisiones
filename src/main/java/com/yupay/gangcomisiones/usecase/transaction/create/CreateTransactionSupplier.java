c8ef015346c1792a98dff4a89c9a49415b3b9d19
dvidal <dvidal@infoyupay.com>
2025-09-15 07:24:43 -0500

    feat: add `CreateTransactionSupplier` for managing `ManageConceptController`
    Introduced `CreateTransactionSupplier` to provide default wiring of `AppContext` and `UseCaseControllerRegistry` for `ManageConceptController`.

---
+++ src/main/java/com/yupay/gangcomisiones/usecase/transaction/create/CreateTransactionSupplier.java
@@ -0,0 +1,44 @@
+/*
+ * gang-comisiones
+ * COPYLEFT 2025
+ * Ingenieria Informatica Yupay SACS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ *  with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.yupay.gangcomisiones.usecase.transaction.create;
+
+import com.yupay.gangcomisiones.AppContext;
+import com.yupay.gangcomisiones.usecase.concept.manage.ManageConceptController;
+import com.yupay.gangcomisiones.usecase.registry.UseCaseControllerRegistry;
+import com.yupay.gangcomisiones.usecase.registry.UseCaseSupplier;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Supplier for {@link CreateTransactionController} that wires the default {@link AppContext}
+ * and the default {@link UseCaseControllerRegistry}.
+ *
+ * @author InfoYupay SACS
+ * @version 1.0
+ */
+public class CreateTransactionSupplier implements UseCaseSupplier<ManageConceptController> {
+    @Override
+    public ManageConceptController get() {
+        Supplier<AppContext> appContext = () -> Objects.requireNonNull(appContextSupplier().get(),
+                () -> nullPointerMessage(ManageConceptController.class, AppContext.class));
+        return new ManageConceptController(appContext, controllerRegistry().get());
+    }
+}
