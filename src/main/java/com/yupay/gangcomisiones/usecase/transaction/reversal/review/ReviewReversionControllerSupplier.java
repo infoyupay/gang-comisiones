e97941f97bf8f4d957310d6dd0a1b7615350fd2e
dvidal <dvidal@infoyupay.com>
2025-09-16 12:28:31 -0500

    feat: add `ReviewReversionControllerSupplier` for default wiring
    Introduced `ReviewReversionControllerSupplier` to provide instances of `ReviewReversionController` with default `AppContext` configuration.

---
+++ src/main/java/com/yupay/gangcomisiones/usecase/transaction/reversal/review/ReviewReversionControllerSupplier.java
@@ -0,0 +1,41 @@
+/*
+ * gang-comisiones
+ * COPYLEFT 2025
+ * Ingenieria Informatica Yupay SACS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ *  with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.yupay.gangcomisiones.usecase.transaction.reversal.review;
+
+import com.yupay.gangcomisiones.AppContext;
+import com.yupay.gangcomisiones.usecase.registry.UseCaseSupplier;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Supplier for {@link ReviewReversionController} that wires the default {@link AppContext}.
+ *
+ * @author InfoYupay SACS
+ * @version 1.0
+ */
+public class ReviewReversionControllerSupplier implements UseCaseSupplier<ReviewReversionController> {
+    @Override
+    public ReviewReversionController get() {
+        Supplier<AppContext> appContext = () -> Objects.requireNonNull(appContextSupplier().get(),
+                () -> nullPointerMessage(ReviewReversionController.class, AppContext.class));
+        return new ReviewReversionController(appContext.get());
+    }
+}
