3f3304ea72050b42e6272024325249fb56241af1
dvidal <dvidal@infoyupay.com>
2025-09-24 19:38:14 -0500

    feat: add `AppMode` enum to represent application operational modes
    Introduced `AppMode` enum with `WORK`, `TOY`, and `GHOST` modes to distinguish between production and test environments. Includes a utility method for string conversion to default mode (`WORK`) on invalid input.

---
+++ src/main/java/com/yupay/gangcomisiones/AppMode.java
@@ -0,0 +1,88 @@
+/*
+ * gang-comisiones
+ * COPYLEFT 2025
+ * Ingenieria Informatica Yupay SACS
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ *  with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.yupay.gangcomisiones;
+
+/**
+ * The {@code AppMode} enum represents different operational modes of an application.
+ * These modes determine how the application interacts with data and its underlying structure.
+ * <br/><br/>
+ * Available modes:
+ * <ul>
+ *     <li>{@link #WORK} - The application operates on production data.</li>
+ *     <li>
+ *         {@link #TOY} - The application interacts with a test database,
+ *         simulating real workflow without affecting production.
+ *     </li>
+ *     <li>
+ *         {@link #GHOST} - The application works with temporary test data that is deleted after execution,
+ *         giving a fresh state for each session.
+ *     </li>
+ * </ul>
+ *
+ * @author InfoYupay SACS
+ * @version 1.0
+ */
+public enum AppMode {
+    /**
+     * Represents the real work application mode, it'll make point
+     * the app to real - production data.
+     */
+    WORK,
+    /**
+     * Represents the toy application mode, it'll make point
+     * the app to test data, persisted in a real database, but
+     * such database is a test database, so any input won't impact
+     * the real workflow.
+     */
+    TOY,
+    /**
+     * Represents the ghost application mode, it'll make point
+     * the app to test data, persisted in a real database, but
+     * such database and any persisted file will be deleted after
+     * app execution. Each time the app is started in this mode
+     * is like the first time.
+     */
+    GHOST;
+
+    /**
+     * Converts a given string to its corresponding {@link AppMode} enumeration constant.
+     * The input string is normalized by removing leading and trailing whitespaces and converting it to uppercase.
+     * If the conversion fails (e.g., due to an invalid value), the default {@link AppMode#WORK} is returned.
+     *
+     * @param value The string representation of the desired {@link AppMode}.
+     *              <ul>
+     *                  <li>This value is case-insensitive.</li>
+     *                  <li>Leading and trailing whitespaces are ignored.</li>
+     *              </ul>
+     *              <br/>
+     *              If the value does not match any {@link AppMode}, the method will return {@link AppMode#WORK}.
+     * @return The corresponding {@link AppMode} constant if the conversion is successful.
+     * <ul>
+     *     <li>Returns {@link AppMode#WORK} if the input is invalid or null.</li>
+     * </ul>
+     */
+    public static AppMode from(String value) {
+        try {
+            return AppMode.valueOf(value.strip().toUpperCase());
+        } catch (Exception _) {
+            return WORK;
+        }
+    }
+}
